bde8d3f506cc9c295bbcc29e241ab77f735f2583
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2020-08-02 21:48:43 +0200

    style: use object name -> id enum instead of hardcoded ids

--- yolo_labels/vott/main.py
+++ yolo_labels/vott/main.py
@@ -1,3 +1,4 @@
+from yolo_labels.shared import ObjectNameId
 from yolo_labels.vott.reader import VoTTLabelReader
 from yolo_labels.shared import LabelWriter
 import os
@@ -9,7 +10,7 @@ print(input_path)
 assert os.path.isfile(input_path)
 
 reader = VoTTLabelReader(input_path=input_path,
-                         label_id_mapper={'moped_bbox': 0},
+                         label_id_mapper={'moped_bbox': ObjectNameId.E_SCOOTER},
                          object_labels='moped_bbox',
                          ignore_unmapped_labels=True)
 writer = LabelWriter(output_path='dist/output.txt', reader=reader, overwrite_existent=True)
