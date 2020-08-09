44417622c73517f401a212121436f4d97a879872
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2020-08-09 16:33:13 +0200

    feat: implement rel to abs path

---
+++ yolo_labels/shared/main.py
@@ -0,0 +1,5 @@
+from yolo_labels.shared.relative_to_absolute import RelativeToAbsolute
+
+
+rel_to_abs = RelativeToAbsolute('dist/output.txt', 'dist/output_abs.txt', '/Users/bothmena/Projects')
+rel_to_abs()
