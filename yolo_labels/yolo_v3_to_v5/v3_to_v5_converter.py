3583472ef3ed1ddea7898e0fdb2c09b64f207e70
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2020-08-11 09:12:34 +0200

    feat: create correct folder structure, copy images files

--- yolo_labels/yolo_v3_to_v5/v3_to_v5_converter.py
+++ yolo_labels/yolo_v3_to_v5/v3_to_v5_converter.py
@@ -1,6 +1,7 @@
 import io
-from os import path
+import os
 from PIL import Image
+import shutil
 
 from yolo_labels.yolo_v3_to_v5 import YoloV5DatasetType
 
@@ -17,8 +18,11 @@ class V3ToV5Converter:
         self.images_dir = images_dir
         self.output_dir = output_dir
         self.dataset_type = dataset_type.value
+        self.output_img_dir = None
+        self.output_labels_dir = None
 
     def __call__(self):
+        self.create_dirs()
         with io.open(self.input_file, 'r') as input_file:
             for line in input_file.readlines():
                 self.write_v5_annotations(line)
@@ -26,13 +30,17 @@ class V3ToV5Converter:
     def write_v5_annotations(self, line: str):
         image, *objects = line.split(' ')
         annotations = self.generate_annotations(image, objects)
-        output_filename = '{}.txt'.format(path.splitext(image)[0])
-        with io.open(path.join(self.output_dir, output_filename), 'w+') as output_file:
+        output_filename = '{}.txt'.format(os.path.splitext(image)[0])
+        with io.open(os.path.join(self.output_labels_dir, output_filename), 'w+') as output_file:
             output_file.write(annotations)
+        self.copy_image_file(image)
 
     def generate_annotations(self, image, objects: list):
         annotations = ''
-        for obj in objects:
+        for obj in objects:  # type: str
+            obj = obj.strip()
+            if obj == '':
+                continue
             annotations += self.stringify_object_data(image, obj)
 
         return annotations
@@ -46,8 +54,21 @@ class V3ToV5Converter:
         return ' '.join(annotation_list) + '\n'
 
     def normalize_bbox(self, image_filename: str, bbox: list):
-        image = Image.open(path.join(self.images_dir, image_filename))  # type: Image.Image
+        image = Image.open(os.path.join(self.images_dir, image_filename))  # type: Image.Image
         height, width = image.size
         float_bbox = [float(item) for item in bbox]
 
         return [float_bbox[0]/width, float_bbox[1]/height, float_bbox[2]/width, float_bbox[3]/height]
+
+    def create_dirs(self):
+        os.mkdir(os.path.join(self.output_dir, self.dataset_type))
+        self.output_img_dir = os.path.join(self.output_dir, self.dataset_type, 'images')
+        os.mkdir(self.output_img_dir)
+        self.output_labels_dir = os.path.join(self.output_dir, self.dataset_type, 'labels')
+        os.mkdir(self.output_labels_dir)
+
+    def copy_image_file(self, image):
+        shutil.copy2(
+            os.path.join(self.images_dir, image),
+            os.path.join(self.output_img_dir, image)
+        )
