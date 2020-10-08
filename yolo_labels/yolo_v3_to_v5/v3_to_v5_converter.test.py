c91b291c07241cbe7dfae604a1aa1a919c585d6f
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2020-10-08 11:50:51 +0200

    test: fix running tests with unittest

--- yolo_labels/yolo_v3_to_v5/v3_to_v5_converter.test.py
+++ yolo_labels/yolo_v3_to_v5/v3_to_v5_converter.test.py
@@ -1,8 +1,8 @@
-# import unittest
+import unittest
 from yolo_labels.yolo_v3_to_v5 import YoloV5DatasetType, V3ToV5Converter
 
 
-class ConverterTestSuit: # (unittest.TestCase):
+class ConverterTestSuit(unittest.TestCase):
     def __init__(self):
         super(ConverterTestSuit, self).__init__()
         self.triple_sizes = {
@@ -20,13 +20,10 @@ class ConverterTestSuit: # (unittest.TestCase):
         for key, lines_part in split_lines.items():
             sum += len(lines_part)
 
-        assert sum == len(lines), 'sum of all lines ({}) should match sum of all split lines ({})'\
-            .format(len(lines), sum)
-        assert len(split_lines[YoloV5DatasetType.TEST]) == 101, 'sum of test lines ({}) should be == 101'\
-            .format(len(split_lines[YoloV5DatasetType.TEST]))
+        self.assertEqual(sum, len(lines))
+        self.assertEqual(len(split_lines[YoloV5DatasetType.TEST]), 101)
 
 
 if __name__ == '__main__':
-    print('starting test suite for converter...')
     conv_test = ConverterTestSuit()
     conv_test.test_split_lines_sum_equal_1()
