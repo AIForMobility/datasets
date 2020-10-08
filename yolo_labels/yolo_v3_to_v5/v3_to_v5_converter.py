1ce4f0a43c47b3991ec219c62705ed591a8ade29
Aymen Ben Othmen <aymenbenothmenabo@gmail.com>
2020-10-08 11:47:58 +0200

    feat: implement function to write data.yaml file

--- yolo_labels/yolo_v3_to_v5/v3_to_v5_converter.py
+++ yolo_labels/yolo_v3_to_v5/v3_to_v5_converter.py
@@ -2,7 +2,8 @@ import io
 import os
 import random
 import shutil
-from typing import List, Dict
+from typing import List, Dict, Type
+from enum import Enum
 
 from PIL import Image
 
@@ -15,12 +16,14 @@ class V3ToV5Converter:
             input_file: str,
             images_dir: str,
             output_dir: str,
-            sizes: Dict[YoloV5DatasetType, float]
+            sizes: Dict[YoloV5DatasetType, float],
+            normalize_bboxes: bool = False,
     ):
         self.input_file = input_file
         self.images_dir = images_dir
         self.output_dir = output_dir
         self.sizes = sizes
+        self.normalize_bboxes = normalize_bboxes
 
     def __call__(self):
         self.create_dirs(list(self.sizes.keys()))
@@ -35,13 +38,13 @@ class V3ToV5Converter:
         for line in lines:
             self.write_v5_annotations(line, labels_dir, images_dir)
 
-    def write_v5_annotations(self, line: str, labels_dir: str, images_dir: str):
+    def write_v5_annotations(self, line: str, labels_dir: str, output_images_dir: str):
         image, *objects = line.split(' ')
         annotations = self.generate_annotations(image, objects)
         output_filename = '{}.txt'.format(os.path.splitext(image)[0])
         with io.open(os.path.join(labels_dir, output_filename), 'w+') as output_file:
             output_file.write(annotations)
-        self.copy_image_file(images_dir, image)
+        self.copy_image_file(output_images_dir, image)
 
     def generate_annotations(self, image, objects: list):
         annotations = ''
@@ -62,10 +65,13 @@ class V3ToV5Converter:
         return ' '.join(annotation_list) + '\n'
 
     def normalize_bbox(self, image_filename: str, bbox: list):
-        image = Image.open(os.path.join(self.images_dir, image_filename))  # type: Image.Image
-        height, width = image.size
         float_bbox = [float(item) for item in bbox]
 
+        if not self.normalize_bboxes:
+            return float_bbox
+
+        image = Image.open(os.path.join(self.images_dir, image_filename))  # type: Image.Image
+        height, width = image.size
         return [float_bbox[0] / width, float_bbox[1] / height, float_bbox[2] / width, float_bbox[3] / height]
 
     def create_dirs(self, dataset_types: List[YoloV5DatasetType]):
@@ -74,10 +80,10 @@ class V3ToV5Converter:
             os.mkdir(os.path.join(self.output_dir, dataset_type.value, 'images'))
             os.mkdir(os.path.join(self.output_dir, dataset_type.value, 'labels'))
 
-    def copy_image_file(self, image_dir: str, image: str):
+    def copy_image_file(self, output_image_dir: str, image: str):
         shutil.copy2(
             os.path.join(self.images_dir, image),
-            os.path.join(image_dir, image)
+            os.path.join(output_image_dir, image)
         )
 
     def split_lines(self, lines: List[str]) -> Dict[YoloV5DatasetType, List[str]]:
@@ -92,3 +98,12 @@ class V3ToV5Converter:
             split_lines[dataset_type] = lines[start_index:end_index]
 
         return split_lines
+
+    def write_data_yaml_file(self, labels: Type[Enum]):
+        classes_names = [cls.name for cls in labels]
+        with open(os.path.join(self.output_dir, 'data.yaml'), 'w') as f:
+            f.write('train: ../train/images\n')
+            f.write('val: ../valid/images\n')
+            f.write('\n')
+            f.write('nc: {}\n'.format(len(classes_names)))
+            f.write("names: {}".format(classes_names))
